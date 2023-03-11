3c5e29cefcd64d7da8d5924c3fe43a09130f5a48
Shashank <pshashank64@gmail.com>
2023-03-12 01:02:47 +0530

    basic form done

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> hello </h1>
-        
+        <h1> Hello, please fill the form to proceed!! </h1>
+        <form action='' method='POST' className='formData'>
+          <input type="text" placeholder='Enter your name'></input>
+          <input type="date"></input>
+          <input type="email" placeholder='Enter your Email'></input>
+          <input type="number" placeholder='Enter your Number'></input>
+        </form>
       </header>
     </div>
   );
